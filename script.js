f94cfc2b4863906e10be34c6901cf751461daa09
kibeert <ckibertkoech@gmail.com>
2024-02-09 16:33:21 +0300

    Added Ford to the list of cars. Added a function to calculate the full name of a person. (generated by blackbox.ai )

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ console.log("The make of the car is: "+car);
 // Output: The make of the car is: Toyota
 cars[0]="Ford"
 console.log("After adding Ford to the list, the makes are now: " + cars)
+
+const person = {
+    'fname':'John',
+    'lname':"Doe",
+    'age':20
+}
+let fname = person.fname
+let lname = person.lname
+function calfulname(fname, lname){
+    return fname +" "+  lname
+}
+console.log(person.fname)
+console.log(calfulname(fname, lname))
