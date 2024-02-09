cbaa807957fa213b116c1438302832e5da8361ce
kibeert <ckibertkoech@gmail.com>
2024-02-09 17:49:23 +0300

    Add various JavaScript functions and console logs to the script.js file. - Added a function to calculate the full name of a person. - Logged the first name of the person. - Logged the result of calling the calculate full name function. - Added an array of fruits and logged various operations on the array. - Logged the current date and time using different Date() methods. - Logged the result of parsing a date string. - Added a function to generate a random number between a specified range. - Logged a random number between 1 and 68. - Logged a boolean value and its type. (generated by blackbox.ai )

--- script.js
+++ script.js
@@ -18,3 +18,41 @@ function calfulname(fname, lname){
 }
 console.log(person.fname)
 console.log(calfulname(fname, lname))
+
+const fruits= ["Banana", "Apple", "Mango", "Orange"]
+console.log(fruits.join(" * "))
+console.log(fruits)
+console.log(fruits.toString(fruits))
+console.log(fruits.pop())
+console.log(fruits)
+console.log(fruits.push("mango"))
+console.log(fruits)
+
+//DATES
+const d = new Date()
+const de = Date()
+console.log(d)
+console.log(de)
+const e = Date("year", "month")
+console.log(e.toString())
+const t = Date()
+s = t.toString()
+console.log(t)
+
+//Date Formats
+//1.ISO date eg = 2105-03-25
+//2.Short date eg 
+
+let msec = Date.parse("March 21 , 2012")
+console.log(msec)
+//Math functions
+let f = (Math.random() * 100) + 1
+console.log(f)
+
+function getrnd(min, max){
+    return Math.floor(Math.random() * (max-min) + min) 
+}
+let y = getrnd(1, 68)
+console.log(y)
+let x = true
+console.log(x , typeof(x))
