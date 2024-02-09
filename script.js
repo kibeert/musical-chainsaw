1484b0102a2a4596646824b067abd8f480c91969
kibeert <ckibertkoech@gmail.com>
2024-02-09 16:27:31 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const cars =[ "volvo", "Audi", "Toyota"]
+console.log(cars)
+let car = cars[2]
+console.log("The make of the car is: "+car); 
+// Output: The make of the car is: Toyota
+cars[0]="Ford"
+console.log("After adding Ford to the list, the makes are now: " + cars)
